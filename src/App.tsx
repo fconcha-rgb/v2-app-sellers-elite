beec53e03c9bd9f2ffe8652e074a3fb7c0011752
fconcha@falabella.cl <fconcha@falabella.cl>
2026-04-23 18:59:35 -0400

    s

--- src/App.tsx
+++ src/App.tsx
@@ -520,7 +520,6 @@ export default function App() {
 
   const [dashView, setDashView] = useState<ViewMode>('monthly');
   const [user, setUser] = useState<any>({ email: 'open' });
-  const [authLoading, setAuthLoading] = useState(false);
   const [authView, setAuthView] = useState<'login' | 'reset'>('login');
   const [authEmail, setAuthEmail] = useState('');
   const [authPass, setAuthPass] = useState('');
